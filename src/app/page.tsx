ac24cdce5bfd1577e81111ff4022e4a61e0c21fd
SergGrey1992 <sergdiag19@gmail.com>
2024-07-01 10:53:17 +0300

    deploy with api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@ import styles from './page.module.css'
 
 import { getData } from '@/shared/config/server/getData'
 import { GameField } from '@/features/GameField'
-import { Info } from '@/features/Info'
 
 export default async function Home() {
-    // const { data } = await getData()
+    const { data } = await getData()
     return (
         <>
             <main className={styles.main}>
-                <GameField groups={{ blue: [], red: [] }} />
+                <GameField groups={data} />
             </main>
         </>
     )
